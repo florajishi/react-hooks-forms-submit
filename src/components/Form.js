2e0796d0b5642c92e72ac7e5e550780571d73bec
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-06-22 12:14:42 -0500

    revise onSubmit event, add display for all submitted data

--- src/components/Form.js
+++ src/components/Form.js
@@ -24,12 +24,24 @@ function Form() {
     setFirstName("")
     setLastName("")
   }
+
+  const listOfSubmissions = submittedData.map((data,index) => {
+    return(
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    )
+  })
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit" onSubmit={handleSubmit}>Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
   );
 }
 
